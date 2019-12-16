c21318267fb35edc132c97d5d9dd082c05a32e98
igor Ciqueira Silva <Igor@igors-MacBook-Pro.local>
2019-12-16 16:39:33 -0200

    Ajuste dos campos e filtro de valor

--- app/src/main/java/com/igor/easy/view/investment/InvestmentFragment.kt
+++ app/src/main/java/com/igor/easy/view/investment/InvestmentFragment.kt
@@ -12,12 +12,14 @@ import kotlinx.android.synthetic.main.fragment_investment.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 import com.igor.easy.helper.InputFilterMinMax
 import android.text.InputFilter
-
-
+import java.text.SimpleDateFormat
+import java.util.*
+import android.app.DatePickerDialog
 
 class InvestmentFragment : Fragment() {
 
     private val mViewModel by sharedViewModel<InvestmentViewModel>()
+    val myCalendar = Calendar.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -40,23 +42,50 @@ class InvestmentFragment : Fragment() {
     }
 
     private fun actions() {
+
+        //value to investment
         invoice.afterTextChanged {
             mViewModel.setInvoice(it)
             mViewModel.listenFields()
         }
-        maturity_date.afterTextChanged {
-            mViewModel.setMaturity(it)
-            mViewModel.listenFields()
+
+        //date calendar
+        val date = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, monthOfYear)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel()
         }
+        maturity_date.setOnFocusChangeListener { _, hasFocus ->
+            if(hasFocus)
+                context?.let {
+                    DatePickerDialog(it, date, myCalendar
+                            .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                            myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+                }
+        }
+
+        //rate
         rate.afterTextChanged {
             mViewModel.setRate(it)
             mViewModel.listenFields()
         }
         rate.filters = arrayOf<InputFilter>(InputFilterMinMax(1, 100))
 
+
+        //button to result
         btn_simulate.setOnClickListener {
             mViewModel.simulate()
         }
+    }
+
+    private fun updateLabel() {
+        val myFormat = "yyyy-MM-dd" //In which you need put here
+        val sdf = SimpleDateFormat(myFormat, Locale("pt", "BR"))
+
+        maturity_date.setText(sdf.format(myCalendar.time))
+        mViewModel.setMaturity(sdf.format(myCalendar.time))
+        mViewModel.listenFields()
     }
 
     companion object {
